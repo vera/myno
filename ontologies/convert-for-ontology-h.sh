92324a6b02429fd4a19bac2ae3f9bf91388d1a33
Vera Clemens <clemens@uni-potsdam.de>
2022-02-01 20:36:27 +0100

    ontologies: Add script for shrinking ontologies

---
+++ ontologies/convert-for-ontology-h.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./convert-for-ontology-h.sh PATH-TO-ONTOLOGY"
+    echo "Removes all extra spaces, newlines and escapes quotation marks."
+    exit 1
+fi
+
+# Source for awk command: https://stackoverflow.com/a/17302816
+cat $1 | awk '
+    BEGIN {FS = OFS = "\""}
+    /^[[:blank:]]*$/ {next}
+    {for (i=1; i<=NF; i+=2) gsub(/[[:space:]]/,"",$i)} 
+    1
+' | tr -d '\n' | sed 's/"/\\"/g'
+echo "\n"
